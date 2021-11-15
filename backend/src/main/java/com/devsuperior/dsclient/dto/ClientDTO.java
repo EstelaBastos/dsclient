45e2b9276fa0fa9278508a5dfb0e1e301a23716f
Estela Bastos <eombastos@gmail.com>
2021-11-15 14:11:57 -0300

    add id to dto, client resource, exception handling

--- backend/src/main/java/com/devsuperior/dsclient/dto/ClientDTO.java
+++ backend/src/main/java/com/devsuperior/dsclient/dto/ClientDTO.java
@@ -8,6 +8,7 @@ import com.devsuperior.dsclient.entities.Client;
 public class ClientDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	private Long id;
 	private String name;
 	private String cpf;
 	private Double income;
@@ -17,7 +18,8 @@ public class ClientDTO implements Serializable {
 	public ClientDTO() {
 	}
 
-	public ClientDTO(String name, String cpf, Double income, Instant birthDate, Integer children) {
+	public ClientDTO(Long id, String name, String cpf, Double income, Instant birthDate, Integer children) {
+		this.id = id;
 		this.name = name;
 		this.cpf = cpf;
 		this.income = income;
@@ -26,11 +28,20 @@ public class ClientDTO implements Serializable {
 	}
 	
 	public ClientDTO(Client entity) {
+		this.id = entity.getId();
 		this.name = entity.getName();
 		this.cpf = entity.getCpf();
 		this.income = entity.getIncome();
 		this.birthDate = entity.getBirthDate();
 		this.children = entity.getChildren();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 	public String getName() {
